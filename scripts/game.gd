ca342d00ee381aa536f33eec83a3b181d7732889
Brett Haines <bhaines3@gatech.edu>
2019-08-11 00:16:07 -0400

    Addition initial version of SJT algorithm

--- scripts/game.gd
+++ scripts/game.gd
@@ -3,11 +3,15 @@ extends Node2D
 const Cluster = preload("flower_cluster.gd")
 const Connection = preload("connection.gd")
 
+const SJTE = preload("sjte_alg.gd")
+
 # List of all flower clusters in the level
 var clusters = []
 
 func _ready():
+	clusters.append($ui_layer/plunger_btn)
 	generate_clusters(5)
+	SJTE.sjte(len(clusters)-1)
 
 # Generate a number of flower clusters and place them about the screen randomly
 func generate_clusters(num):
@@ -28,3 +32,28 @@ func connect_clusters(src_cluster, dest_cluster):
 	var cable = Connection.new()
 	cable._init_(src_cluster, dest_cluster)
 	$line_layer.call_deferred("add_child", cable)
+
+# Calculate the shortest Hamiltonian path
+func calculate_min_path():
+	# First generate a table of distances between all nodes
+	var dists = []
+	for i in range(len(clusters)):
+		dists.append([])
+		for j in range(len(clusters)):
+			dists[i][j] = clusters[i].rect_position.distance_to(
+					clusters[j].rect_position)
+	
+	# Use Steinhaus-Johnson-Trotter alg to generate all paths
+	var permutations = SJTE.sjte(len(clusters)-1)
+	
+	# Iterate over all paths that start at 0, keeping track of current minimum
+	# See Steinhaus-Johnson-Trotter algorithm
+	var min_path = []
+	var min_length = INF
+
+# Get the length of a given path
+func calculate_path_length(length_table, path):
+	var length = 0
+	for i in range(len(path)-1):
+		length += length_table[path[i]][path[i+1]]
+	return length
---
+++ scripts/sjte_alg.gd
@@ -0,0 +1,68 @@
+extends Node
+
+# Collection of static functions for the Steinhaus-Johnson-Trotter algorithm
+# with Even's speedup.
+# https://en.wikipedia.org/wiki/Steinhaus-Johnson-Trotter_algorithm
+
+# SJT algorithm; returns a list of lists representing all permutations of
+# numbers from 1 to N
+static func sjte(N):
+	
+	# List of permutations to return
+	var permutations = []
+	
+	# Initialize the list of numbers and the directions
+	# In directions list, -1 means left and 1 means right, and 0 is undirected
+	var directions = [0]
+	var indicies = [1]
+	for i in range(N-1):
+		directions.append(-1)
+		indicies.append(i+2)
+	
+	# Continue as long as there is a "mobile" (not undirected) number
+	var has_mobile = true
+	while has_mobile:
+		# Find the largest mobile number's index
+		var max_mobile = -INF
+		for i in range(N):
+			if directions[i] != 0 and (max_mobile == -INF or indicies[i] > indicies[max_mobile]):
+				max_mobile = i
+		if max_mobile == -INF:
+			has_mobile = false
+			continue
+		
+		# Swap the largest mobile index with the neighbor it's looking at
+		swap(indicies, max_mobile, max_mobile + directions[max_mobile])
+		swap(directions, max_mobile, max_mobile + directions[max_mobile])
+		max_mobile += directions[max_mobile]
+		
+		# If max_mobile is now first or last, it's direction changes to 0
+		if max_mobile == 0 or max_mobile == N-1:
+			directions[max_mobile] = 0
+		# Additionally, if new neighbor it's looking at is bigger, direction changes to 0
+		elif indicies[max_mobile] < indicies[max_mobile+directions[max_mobile]]:
+			directions[max_mobile] = 0
+		
+		# Change the direction of all numbers larger than the largest mobile
+		# number with no direction so that they point toward max_mobile
+		for i in range(N):
+			if indicies[i] > indicies[max_mobile] and directions[i] == 0:
+				if i < max_mobile:
+					directions[i] = 1
+				else:
+					directions[i] = -1
+		
+		# Append a copy of the current permutation to list
+		permutations.append(indicies.duplicate(true))
+		
+		# DEBUG: Print current permutation
+		print("Current permutation: " + str(indicies))
+	
+	# When algorithm terminates, return list of permuatations
+	return permutations
+
+# Swap 2 elements of a given list
+static func swap(list, i, j):
+	var temp = list[i]
+	list[i] = list[j]
+	list[j] = temp
