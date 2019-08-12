12471aa2485519e5da984cc6feab2d1ae6393d3b
Brett Haines <bhaines3@gatech.edu>
2019-08-11 21:32:56 -0400

    Fixing bug in SJT algorithm

--- scripts/sjte_alg.gd
+++ scripts/sjte_alg.gd
@@ -19,6 +19,9 @@ static func sjte(N):
 		directions.append(-1)
 		indicies.append(i+2)
 	
+	# Add the initial permutation to the list
+	permutations.append(indicies.duplicate(true))
+	
 	# Continue as long as there is a "mobile" (not undirected) number
 	var has_mobile = true
 	while has_mobile:
@@ -28,19 +31,21 @@ static func sjte(N):
 			if directions[i] != 0 and (max_mobile == -INF or indicies[i] > indicies[max_mobile]):
 				max_mobile = i
 		if max_mobile == -INF:
+			# No mobile numbers; break out of loop
 			has_mobile = false
 			continue
 		
 		# Swap the largest mobile index with the neighbor it's looking at
+		var new_max_mobile = max_mobile + directions[max_mobile]
 		swap(indicies, max_mobile, max_mobile + directions[max_mobile])
 		swap(directions, max_mobile, max_mobile + directions[max_mobile])
-		max_mobile += directions[max_mobile]
+		max_mobile = new_max_mobile
 		
 		# If max_mobile is now first or last, it's direction changes to 0
 		if max_mobile == 0 or max_mobile == N-1:
 			directions[max_mobile] = 0
 		# Additionally, if new neighbor it's looking at is bigger, direction changes to 0
-		elif indicies[max_mobile] < indicies[max_mobile+directions[max_mobile]]:
+		if indicies[max_mobile] < indicies[max_mobile+directions[max_mobile]]:
 			directions[max_mobile] = 0
 		
 		# Change the direction of all numbers larger than the largest mobile
@@ -54,9 +59,6 @@ static func sjte(N):
 		
 		# Append a copy of the current permutation to list
 		permutations.append(indicies.duplicate(true))
-		
-		# DEBUG: Print current permutation
-		print("Current permutation: " + str(indicies))
 	
 	# When algorithm terminates, return list of permuatations
 	return permutations
