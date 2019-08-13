7933f5e9cbafc96fa1d411ca272066de66d4073c
Brett Haines <bhaines3@gatech.edu>
2019-08-13 17:55:30 -0400

    Removing shape-drawing code from cluster script

--- scripts/flower_cluster.gd
+++ scripts/flower_cluster.gd
@@ -8,14 +8,5 @@ func _ready():
 	max_connections = 2
 	available_connections = 2
 	
-	# Create a shape to display
-	var shape = Sprite.new()
-	call_deferred("add_child", shape)
-	shape.set_texture(load("res://assets/images/patch_base.exr"))
-	shape.centered = false
-	
-	rect_size = shape.get_texture().get_size()
-	rect_scale = Vector2(0.1, 0.1)
-	
 	# Get input
 	set_process_input(true)
