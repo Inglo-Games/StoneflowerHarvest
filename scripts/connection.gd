884005dc5f7352e1a5c4263871c46b5d76d7d315
Brett Haines <bhaines3@gatech.edu>
2019-08-19 18:54:16 -0400

    Adding texture for cord line

--- scripts/connection.gd
+++ scripts/connection.gd
@@ -1,7 +1,6 @@
 extends Line2D
 
 # This class defines the behavior of a connection between two patches
-
 class_name Connection
 
 var nodes = []
@@ -18,6 +17,18 @@ func _init_(src_cluster, dest_cluster):
 	# Setup endpoints
 	add_point(src_cluster.rect_position + src_cluster.rect_size * src_cluster.rect_scale / 2.0)
 	add_point(dest_cluster.rect_position + dest_cluster.rect_size * dest_cluster.rect_scale / 2.0)
+	
+	# Load pattern image from disk
+	var img = Image.new()
+	img.load("res://assets/images/cord_pattern.png")
+	var tex = ImageTexture.new()
+	tex.create_from_image(img)
+	
+	# Setup line's visuals
+	set_begin_cap_mode(Line2D.LINE_CAP_ROUND)
+	set_end_cap_mode(Line2D.LINE_CAP_ROUND)
+	set_texture_mode(Line2D.LINE_TEXTURE_TILE)
+	set_texture(tex)
 
 # Destroy this connection and update clusters accordingly
 func destroy():
