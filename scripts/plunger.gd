b5f3396b03f91cb5cf76414b81c29d65dc15f37b
Brett Haines <bhaines3@gatech.edu>
2019-08-29 20:06:04 -0400

    Removing commented out function

--- scripts/plunger.gd
+++ scripts/plunger.gd
@@ -16,11 +16,6 @@ func _on_click(ev):
 	# Handle single click
 	if ev is InputEventMouseButton and not ev.pressed:
 		GameRoot.check_solution()
-	# Handle click and drag
-	#elif ev is InputEventMouseMotion \
-	#		and Input.is_mouse_button_pressed(BUTTON_LEFT) \
-	#		and not GameRoot.is_drawing:
-	#	GameRoot.start_drawing(get_viewport().get_mouse_position())
 
 # Function only exists for simplicity in check_solution function
 func fire_explosion():
